124cc1e6cb970a531136a2755cdaf9ba3cb8eeab
tatref <tatref@github.com>
2019-04-26 01:25:35 +0200

    use ncollide3d

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ use std::fs::File;
 use image::ConvertBuffer;
 use na::{Vector3, Vector2, Point3, Unit};
 use na::{U1, U2, U3};
+use ncollide3d::query::Ray;
+use ncollide3d::math::Isometry;
+use ncollide3d::shape::Ball;
+use ncollide3d::query::ray_internal::ray::RayCast;
+use nalgebra::geometry::Isometry3;
 
 
 
@@ -21,42 +26,10 @@ const PRECISION: f32 = 0.00001;
 
 
 
-// TODO: https://ncollide.org/geometric_queries/#ray-casting
-#[derive(Copy, Clone, Debug)]
-struct Ray {
-    org: Vector3<f32>,
-    dir: Unit<Vector3<f32>>,
-}
-
-impl Ray {
-    fn intersect_normal(&self, sphere: &Sphere) -> Option<(f32, Unit<Vector3<f32>>)> {
-        let oc = self.org - sphere.center;
-
-        let a = self.dir.dot(&self.dir);
-        let b = 2. * oc.dot(&self.dir);
-        let c = oc.dot(&oc) - sphere.radius * sphere.radius;
-
-        let delta = b * b - 4. * a * c;
-
-        if delta < 0. {
-            None
-        }
-        else {
-            let t = -b -delta.sqrt() / ( 2. * a);
-            let hit_point = self.org + t * *self.dir;
-            let xx = (hit_point - sphere.center).norm();
-            println!("xx: {}", xx);
-
-            let n = Unit::new_normalize(hit_point - sphere.center);
-            Some((t, n))
-        }
-    }
-}
-
 
 #[derive(Copy, Clone, Debug)]
 struct Camera {
-    org: Vector3<f32>,
+    origin: Point3<f32>,
     dir: Unit<Vector3<f32>>,
     up: Unit<Vector3<f32>>,
     fov: f32,
@@ -64,7 +37,7 @@ struct Camera {
 }
 
 impl Camera {
-    fn get_ray(&self, x: usize, y: usize) -> Ray {
+    fn get_ray(&self, x: usize, y: usize) -> Ray<f32> {
         let x = x as f32;
         let y = y as f32;
 
@@ -72,41 +45,31 @@ impl Camera {
         let dir = *self.dir + *self.up * (y * 2. / self.res.1 as f32 - 1.)
             + left * (x * 2. / self.res.0 as f32 - 1.);
 
-        Ray {
-            org: self.org,
-            dir: Unit::new_normalize(dir),
-        }
+        Ray::new(self.origin, dir.normalize())
     }
 }
 
 
 struct Sphere {
-    center: Vector3<f32>,
-    radius: f32,
+    ball: Ball<f32>,
+    isometry: Isometry<f32>,
     color: image::Rgb<f32>,
 }
-
 impl Sphere {
-    fn new(center: &Vector3<f32>, radius: f32, color : image::Rgb<f32>) -> Self {
+    fn new(ball: Ball<f32>, isometry: Isometry<f32>, color: image::Rgb<f32>) -> Self {
         Self {
-            center: center.clone(),
-            radius,
+            ball,
+            isometry,
             color,
         }
     }
 }
 
-struct Plane {
-    normal: Unit<Vector3<f32>>,
-    dist: f32,
-}
-
 
 struct Scene {
     camera: Camera,
-    spheres: Vec<Sphere>,
+    objects: Vec<Sphere>,
 }
-
 impl Scene {
     fn render(&self) -> image::ImageBuffer<image::Rgb<f32>, Vec<f32>> {
         let back_color: image::Rgb<f32> = image::Rgb([0., 0., 0.]);
@@ -125,11 +88,15 @@ impl Scene {
                 //TODO
                 //let color = self.trace_ray(&ray);
 
-                let mut intersections: Vec<_> = self.spheres.iter().filter_map(|s| ray.intersect_normal(&s).map(|(d, n)| (d, n, s.color))).collect();
-                intersections.sort_unstable_by(|(dist1, _n1, _color1), (dist2, _n2, _color2)| dist1.partial_cmp(dist2).unwrap());
+                let mut intersections: Vec<_> = self.objects.iter().filter_map(
+                    |s| s.ball.toi_and_normal_with_ray(&s.isometry, &ray, true).map(|x| (x, s.color))
+                    //|s| ray.intersect_normal(&s).map(|(d, n)| (d, n, s.color))
+                ).collect();
+
+                intersections.sort_unstable_by(|(inter1, _color1), (inter2, _color2)| inter1.toi.partial_cmp(&inter2.toi).unwrap());
                 match intersections.get(0) {
-                    Some((dist, normal, color)) => {
-                        let coeff = ray.dir.dot(&normal);
+                    Some((inter, color)) => {
+                        let coeff = 1.0; //ray.dir.dot(&inter.normal);
                         let final_color = image::Rgb([color[0] * coeff, color[1] * coeff, color[2] * coeff]);
                         *imgbuf.get_pixel_mut(x as u32, y as u32) = final_color;
                     },
@@ -184,21 +151,21 @@ fn tone_map(img: &image::ImageBuffer<image::Rgb<f32>, Vec<f32>> ) -> image::Imag
 
 fn main() {
     let camera = Camera {
-        org: Vector3::zeros(),
+        origin: Point3::origin(),
         dir: Vector3::x_axis(),
         up: Vector3::y_axis(),
         fov: 1.,
         res: (500, 500),
     };
 
-    let spheres = vec![
-        Sphere::new(&Vector3::new(20., 0., 0.), 1.0, image::Rgb([255., 0., 0.])),
-        Sphere::new(&Vector3::new(20., 10., 0.), 2.0, image::Rgb([255., 255., 255.])),
-        Sphere::new(&Vector3::new(20., 0., 10.), 8.0, image::Rgb([128., 128., 255.])),
+    let objects = vec![
+        Sphere::new(Ball::new(1.0), Isometry3::translation(20., 0., 0.), image::Rgb([255., 0., 0.])),
+        Sphere::new(Ball::new(2.0), Isometry3::translation(20., 10., 0.), image::Rgb([255., 255., 255.])),
+        Sphere::new(Ball::new(8.0), Isometry3::translation(20., 0., 10.), image::Rgb([255., 255., 0.])),
     ];
     let scene = Scene {
         camera,
-        spheres,
+        objects,
     };
 
     let imgbuf = scene.render();
